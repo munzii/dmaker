9402e8ac86b2e4912bd8e3362369bb3300093c92
munzii <munzii422@gmail.com>
2022-04-12 17:25:10 +0900

    Exception

--- src/main/java/com/munzii/programming/dmaker/service/DMakerService.java
+++ src/main/java/com/munzii/programming/dmaker/service/DMakerService.java
@@ -2,6 +2,8 @@ package com.munzii.programming.dmaker.service;
 
 import com.munzii.programming.dmaker.dto.CreateDeveloper;
 import com.munzii.programming.dmaker.entity.Developer;
+import com.munzii.programming.dmaker.exception.DMakerErrorCode;
+import com.munzii.programming.dmaker.exception.DMakerException;
 import com.munzii.programming.dmaker.repository.DeveloperRepository;
 import com.munzii.programming.dmaker.type.DeveloperLevel;
 import com.munzii.programming.dmaker.type.DeveloperSkillType;
@@ -11,6 +13,11 @@ import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 
+import java.util.Optional;
+
+import static com.munzii.programming.dmaker.exception.DMakerErrorCode.DUPLICATED_MEMBER_ID;
+import static com.munzii.programming.dmaker.exception.DMakerErrorCode.LEVEL_EXPERIENCE_YEARS_NOT_MATCHED;
+
 @Service
 @RequiredArgsConstructor
 public class DMakerService {
@@ -18,6 +25,8 @@ public class DMakerService {
 
     @Transactional
     public void createdDeveloper(CreateDeveloper.Request request) {
+        validateCreateDeveloperRequest(request);
+
         Developer developer = Developer.builder()
                 .developerLevel(DeveloperLevel.JUNIOR)
                 .developerSkillType(DeveloperSkillType.FRONT_END)
@@ -27,6 +36,21 @@ public class DMakerService {
                 .build();
 
         developerRepository.save(developer);
+    }
+
+    private void validateCreateDeveloperRequest(CreateDeveloper.Request request) {
+        if(request.getDeveloperLevel() == DeveloperLevel.SENIOR && request.getExperienceYears() <10) {
+            throw new DMakerException(LEVEL_EXPERIENCE_YEARS_NOT_MATCHED);
+        }
+        if(request.getDeveloperLevel() == DeveloperLevel.JUNGNIOR && (request.getExperienceYears() <4 || request.getExperienceYears() >10)) {
+            throw new DMakerException(LEVEL_EXPERIENCE_YEARS_NOT_MATCHED);
+        }
+        if(request.getDeveloperLevel() == DeveloperLevel.JUNIOR && request.getExperienceYears() >4) {
+            throw new DMakerException(LEVEL_EXPERIENCE_YEARS_NOT_MATCHED);
+        }
+
+        developerRepository.findByMemberId(request.getMemberId()).ifPresent((developer -> {throw new DMakerException(DUPLICATED_MEMBER_ID);
+        }));
     }
 
 }
